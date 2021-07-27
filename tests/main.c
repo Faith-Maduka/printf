2392d2cae848d09649ff25cdaaccd225f06aad6b
OVECJOE <ovecjoe123@gmail.com>
2021-07-27 09:53:06 +0100

    Tested printf.c and related files

---
+++ tests/main.c
@@ -0,0 +1,27 @@
+#include <limits.h>
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    int len;
+    int len2;
+    len = _printf("Let's try to printf a simple sentence.\n");
+    len2 = printf("Let's try to printf a simple sentence.\n");
+    _printf("Length:[%d, %i]\n", len, len);
+    printf("Length:[%d, %i]\n", len2, len2);
+    _printf("Negative:[%d]\n", -762534);
+    printf("Negative:[%d]\n", -762534);
+
+    _printf("Int Min: [%d]\n", INT_MIN);
+    printf("Int Min: [%d]\n", INT_MIN);
+
+    _printf("Int Max: [%d]\n", INT_MAX);
+    printf("Int Max: [%d]\n", INT_MAX);
+    return (0);
+}
